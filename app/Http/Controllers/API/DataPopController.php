8722631d39f2d79b69317dd273e38427a996eb28
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2023-08-05 23:03:44 +0700

    Update DataPopController.php

--- app/Http/Controllers/API/DataPopController.php
+++ app/Http/Controllers/API/DataPopController.php
@@ -12,4 +12,19 @@ class DataPopController extends Controller
         $pop = DataPop::all();
         return $pop;
     }
+    
+    function add(request $request){
+        $data_pop = DataPop::create([
+            'POPID' => $request->pop_kode,
+            'POPName' => $request->nama,
+            'POPCoordinat' => $request->koordinat,
+            'POPAddress' => $request->alamat,
+            'kelurahan' => $request->kelurahan,
+            'kecamatan' => $request->kecamatan,
+            'POPKota' => $request->kota,
+            'POPKind' => $request->building,
+            'POPType' => $request->tipe,
+        ]);
+        return $data_pop;
+    }
 }
